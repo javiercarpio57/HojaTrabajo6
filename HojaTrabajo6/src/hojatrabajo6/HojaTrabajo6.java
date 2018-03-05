cd19bbaf06ea1508d91b24b27098a0a0f687054a
sergiomarchena16 <mar16387@uvg.edu.gt>
2018-03-05 12:15:04 -0600

    Update HojaTrabajo6.java

--- HojaTrabajo6/src/hojatrabajo6/HojaTrabajo6.java
+++ HojaTrabajo6/src/hojatrabajo6/HojaTrabajo6.java
@@ -1,17 +1,46 @@
 
 package hojatrabajo6;
 
+import java.io.*;
+import static java.lang.Double.NaN;
+import static java.lang.Double.isNaN;
+import java.util.Scanner;
+
+
 /**
- *
+ * @author Sergio Marchena
  * @author javie
+ * @version 05.03.2018
  */
 public class HojaTrabajo6 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException 
+    {
+         Scanner scan = null;
+         int opcion;
+                 
+        File archivo = new File ("cards_desc.txt");
+        
+//        System.out.println("Elija una opcion: ");
+//        System.out.println("1. Tree.");
+//        System.out.println("2. Linked Hash.");
+//        System.out.println("3. HashTable.");
+//        opcion = scan.nextInt();
+
+        FileReader fr = new FileReader (archivo);
+        BufferedReader br = new BufferedReader(fr);
+        
+        scan = new Scanner(fr);
+        
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] lineArray = line.split("|");
+            System.out.println(line);
+        }
     }
     
 }
